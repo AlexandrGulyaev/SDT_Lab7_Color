b5cd93587d2690aa229e2d0f48d3735d3b8d8b4a
Grand-Admiral Sanya <38395460+AlexandrGulyaev@users.noreply.github.com>
2020-05-25 15:40:19 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace Gulyaev_AG_7_Color
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.trackBarRed = new System.Windows.Forms.TrackBar();
+            this.trackBarGreen = new System.Windows.Forms.TrackBar();
+            this.trackBarBlue = new System.Windows.Forms.TrackBar();
+            this.labelRed = new System.Windows.Forms.Label();
+            this.labelGreen = new System.Windows.Forms.Label();
+            this.labelBlue = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarRed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarGreen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarBlue)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(222, 222);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseHover += new System.EventHandler(this.panel1_MouseHover);
+            // 
+            // trackBarRed
+            // 
+            this.trackBarRed.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.trackBarRed.BackColor = System.Drawing.SystemColors.Control;
+            this.trackBarRed.Location = new System.Drawing.Point(278, 12);
+            this.trackBarRed.Maximum = 255;
+            this.trackBarRed.Name = "trackBarRed";
+            this.trackBarRed.Size = new System.Drawing.Size(370, 56);
+            this.trackBarRed.TabIndex = 1;
+            this.trackBarRed.TickFrequency = 15;
+            this.trackBarRed.Value = 125;
+            this.trackBarRed.Scroll += new System.EventHandler(this.trackBar_Scroll);
+            // 
+            // trackBarGreen
+            // 
+            this.trackBarGreen.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.trackBarGreen.BackColor = System.Drawing.SystemColors.Control;
+            this.trackBarGreen.Location = new System.Drawing.Point(278, 93);
+            this.trackBarGreen.Maximum = 255;
+            this.trackBarGreen.Name = "trackBarGreen";
+            this.trackBarGreen.Size = new System.Drawing.Size(370, 56);
+            this.trackBarGreen.TabIndex = 1;
+            this.trackBarGreen.TickFrequency = 15;
+            this.trackBarGreen.Value = 125;
+            this.trackBarGreen.Scroll += new System.EventHandler(this.trackBar_Scroll);
+            // 
+            // trackBarBlue
+            // 
+            this.trackBarBlue.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.trackBarBlue.BackColor = System.Drawing.SystemColors.Control;
+            this.trackBarBlue.Location = new System.Drawing.Point(278, 178);
+            this.trackBarBlue.Maximum = 255;
+            this.trackBarBlue.Name = "trackBarBlue";
+            this.trackBarBlue.Size = new System.Drawing.Size(370, 56);
+            this.trackBarBlue.TabIndex = 1;
+            this.trackBarBlue.TickFrequency = 15;
+            this.trackBarBlue.Value = 125;
+            this.trackBarBlue.Scroll += new System.EventHandler(this.trackBar_Scroll);
+            // 
+            // labelRed
+            // 
+            this.labelRed.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.labelRed.AutoSize = true;
+            this.labelRed.Location = new System.Drawing.Point(240, 12);
+            this.labelRed.Name = "labelRed";
+            this.labelRed.Size = new System.Drawing.Size(34, 17);
+            this.labelRed.TabIndex = 2;
+            this.labelRed.Text = "Red";
+            // 
+            // labelGreen
+            // 
+            this.labelGreen.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.labelGreen.AutoSize = true;
+            this.labelGreen.Location = new System.Drawing.Point(240, 93);
+            this.labelGreen.Name = "labelGreen";
+            this.labelGreen.Size = new System.Drawing.Size(48, 17);
+            this.labelGreen.TabIndex = 2;
+            this.labelGreen.Text = "Green";
+            // 
+            // labelBlue
+            // 
+            this.labelBlue.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.labelBlue.AutoSize = true;
+            this.labelBlue.Location = new System.Drawing.Point(240, 178);
+            this.labelBlue.Name = "labelBlue";
+            this.labelBlue.Size = new System.Drawing.Size(36, 17);
+            this.labelBlue.TabIndex = 2;
+            this.labelBlue.Text = "Blue";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(288, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(607, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "255";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(288, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(16, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(607, 132);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 17);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "255";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(288, 217);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(16, 17);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(607, 217);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(32, 17);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "255";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 253);
+            this.Controls.Add(this.labelBlue);
+            this.Controls.Add(this.labelGreen);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelRed);
+            this.Controls.Add(this.trackBarBlue);
+            this.Controls.Add(this.trackBarGreen);
+            this.Controls.Add(this.trackBarRed);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(700, 300);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Color Picker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarRed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarGreen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarBlue)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TrackBar trackBarRed;
+        private System.Windows.Forms.TrackBar trackBarGreen;
+        private System.Windows.Forms.TrackBar trackBarBlue;
+        private System.Windows.Forms.Label labelRed;
+        private System.Windows.Forms.Label labelGreen;
+        private System.Windows.Forms.Label labelBlue;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gulyaev_AG_7_Color
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void trackBar_Scroll(object sender, EventArgs e)
+        {
+            panel1.BackColor = Color.FromArgb(trackBarRed.Value, trackBarGreen.Value, trackBarBlue.Value);
+            uint color = Convert.ToUInt32(trackBarRed.Value<<16) + Convert.ToUInt32(trackBarGreen.Value<<8) + Convert.ToUInt32(trackBarBlue.Value);
+            string col = "#" + Convert.ToString(color,16);
+            Clipboard.SetText(col);
+        }
+
+        private void panel1_MouseHover(object sender, EventArgs e)
+        {
+            trackBar_Scroll(sender, e);
+            ToolTip t = new ToolTip();
+            t.SetToolTip(panel1, Clipboard.GetText());
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            trackBar_Scroll(sender, e);
+        }
+    }
+}
